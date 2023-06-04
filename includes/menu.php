b3ff2903f26c5e76502024d7a837ac7290a28571
Benquime1985 <benqui@hotmail.es>
2023-06-03 20:42:20 -0600

    repos tres

--- includes/menu.php
+++ includes/menu.php
@@ -3,11 +3,11 @@
         <!-- Responsive navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                <a class="navbar-brand" href="#">SkyGate</a>
+                <a class="navbar-brand" href="index.php">SkyGate</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Inicio</a></li>
                         <li class="nav-item"><a class="nav-link" href="#">Publicaciones</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Administrar</a>
@@ -26,7 +26,7 @@
                             </ul>
                         </li>
                         <li class="nav-item"><a class="nav-link" href="#">Acceder</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Registrarse</a></li>
+                        <li class="nav-item"><a class="nav-link" href="registro.php">Registrarse</a></li>
                         <li class="nav-item"><a class="nav-link" href="#">Salir</a></li>
                     </ul>
                 </div>
---
+++ registro.php
@@ -0,0 +1,7 @@
+<?php 
+    include 'includes/header.php';
+?>
+
+    <form class="form">
+        <h2 class="form_title">Registrar</h2>
+    </form>
