b33f8efc4b5a99103c5535c0a458ce5227228c14
Benquime1985 <benqui@hotmail.es>
2023-06-05 15:54:35 -0600

    repo 10

--- includes/menu.php
+++ includes/menu.php
@@ -7,8 +7,8 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="../index.php">Inicio</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Publicaciones</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="./index.php">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Destinos</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Administrar</a>
                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
@@ -16,15 +16,7 @@
                                 <li><a class="dropdown-item" href="./admin/usuarios.php">Usuarios</a></li>
                             </ul>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Nosotros</a>
-                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Quienes Somos</a></li>
-                                <li><a class="dropdown-item" href="#">Servicios</a></li>
-                                <li><hr class="dropdown-divider" /></li>
-                                <li><a class="dropdown-item" href="#">Contactanos</a></li>
-                            </ul>
-                        </li>
+                        
                         <li class="nav-item"><a class="nav-link" href="acceder.php">Acceder</a></li>
                         <li class="nav-item"><a class="nav-link" href="registro.php">Registrarse</a></li>
                         <li class="nav-item"><a class="nav-link" href="#">Salir</a></li>
