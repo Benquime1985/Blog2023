1e099cca6ab462302ca7ebd7426a361717970041
Benquime1985 <benqui@hotmail.es>
2023-06-08 17:33:44 -0600

    Fin del trabajo

--- includes/publicacion.php
+++ includes/publicacion.php
@@ -12,7 +12,7 @@
           <?php foreach ($publicaciones->listar(0,1) as $publicacion):?>
             <div class="col-md-4">
               <article>
-                <img src="img/articulos/<?=$publicacion->imagen?>" alt="Imagen del artículo">
+                <img src="img/articulos/<?=$publicacion->imagen?>" alt="Imagen del artículo" style="width: 450px; height: 200px;" >
                 <h2><?=$publicacion->titulo?></h2>
                 <p>Fecha de creación: <span><?=formatearFecha($publicacion->fecha_creacion)?></span></p>
                 <exp><?=textoCorto($publicacion->texto,100)?></p>
